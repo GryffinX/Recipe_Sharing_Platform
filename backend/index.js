752793aa7890d987876286ebf96758b1ed66b8b3
GryffinX <g.ayush2k07@gmail.com>
2025-06-20 12:27:04 +0530

    Added getting all users functionality for ADMIN role, CORS functionality and implemented VERIFY ROLE MIDDLEWARE

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,28 @@
 require('dotenv').config();
 const express=require('express');
 const mongoConnection = require('./config/mongodb');
+const cors = require('cors')
 const recipeRouter= require('./routes/recipe');
 const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user')
 const {authenticate}= require('./middleware/authMiddleware');
-
-
-mongoConnection();
+const {requiredRole} = require('./middleware/verifyRoleMiddleware');
 
 
 const app=express();
 app.use(express.json());
+app.use(cors({
+    origin: '*'
+}))
+
+
+mongoConnection();
 
 const PORT= process.env.PORT || 3000;
 
 app.use('/recipes',recipeRouter);
 app.use('/auth', authRouter);
+app.use('/users', authenticate, requiredRole(['admin']), userRouter);
 
 app.get('/', (req,res) => {
     return res.status(200).send({message: "Server is successfully running"});
