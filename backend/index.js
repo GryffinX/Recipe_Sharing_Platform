68898747c5003e60ba1011d8e236c650788ed1f1
GryffinX <g.ayush2k07@gmail.com>
2025-06-20 14:19:38 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,9 @@ const {requiredRole} = require('./middleware/verifyRoleMiddleware');
 
 const app=express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 
 
 mongoConnection();
